9ce9fef2626fb0e5938acfbdb9338a3a48ced955
DESKTOP-TH8OV4B\임영빈 <dladudqls132@naver.com>
2021-03-29 20:54:51 +0900

    ui 더미 가능한것들만 넣는중

---
+++ Assets/Script/UI_Ammo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Ammo : MonoBehaviour
+{
+    [SerializeField] private Gun gun;
+    [SerializeField] private Text text_ammoCount;
+    [SerializeField] private Image image_lowAmmoCount;
+    [SerializeField] private Image image_reload;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.GetPlayer().GetWeaponGameObject() != null)
+        {
+            if (GameManager.Instance.GetPlayer().GetWeaponGameObject().GetComponent<Gun>() != gun)
+            {
+                gun = GameManager.Instance.GetPlayer().GetWeaponGameObject().GetComponent<Gun>();
+            }
+        }
+
+        if (gun != null)
+        {
+            text_ammoCount.text = gun.GetCurrentAmmoCount().ToString();
+
+            if (gun.GetCurrentAmmoCount() <= gun.GetMaxAmmoCount() / 3)
+            {
+                image_lowAmmoCount.enabled = true;
+
+                if(!gun.GetIsReload())
+                    image_reload.enabled = true;
+                else
+                    image_reload.enabled = false;
+            }
+            else
+            {
+                image_lowAmmoCount.enabled = false;
+                image_reload.enabled = false;
+            }
+        }
+    }
+}
