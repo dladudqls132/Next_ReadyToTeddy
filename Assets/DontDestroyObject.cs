3f9dfa5f7be2a57d5e93e16200afebf38dc08210
DESKTOP-TH8OV4B\임영빈 <dladudqls132@naver.com>
2021-06-03 20:04:19 +0900

    합병

---
+++ Assets/DontDestroyObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyObject : MonoBehaviour
+{
+    private void Awake() { var obj = FindObjectsOfType<DontDestroyObject>(); if (obj.Length == 1) { DontDestroyOnLoad(gameObject); } else { Destroy(gameObject); } }
+}
