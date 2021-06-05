b33097f3592f4cac9b6b1008189db0ab7d1e060b
DESKTOP-TH8OV4B\임영빈 <dladudqls132@naver.com>
2021-06-06 00:57:29 +0900

    사운드

---
+++ Assets/Script/Sound/BGMController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour
+{
+    [SerializeField] private BGMSoundType bgmType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.GetSoundManager().AudioPlayBGM(bgmType, true);
+    }
+}
--- Assets/Script/Sound/SoundInfo.cs
+++ Assets/Script/Sound/SoundInfo.cs
@@ -19,13 +19,34 @@ public enum SoundType
     Laser,
     EnergyBall,
     Warning_Floor,
-    Warning_TypeC
+    Warning_TypeC,
+    Electric
+}
+
+public enum BGMSoundType
+{
+    BGM_Field1,
+    BGM_Boss1
 }
 
 [System.Serializable]
 public class SoundInfos
 {
     public SoundType soundType;
+    public AudioClip clip;
+
+    [Range(0, 1)]
+    public float volume;
+    [Range(0, 3)]
+    public float pitch_min = 1;
+    [Range(0, 3)]
+    public float pitch_max = 1;
+}
+
+[System.Serializable]
+public class BGMSoundInfos
+{
+    public BGMSoundType soundType;
     public AudioClip clip;
 
     [Range(0, 1)]
@@ -53,4 +74,23 @@ public class SoundInfo : ScriptableObject
     }
 
     public SoundInfos[] sounds;
+}
+
+[CreateAssetMenu(fileName = "BGMSoundInfo", menuName = "Scriptable Object Asset/BGMSoundInfo")]
+public class BGMSoundInfo : ScriptableObject
+{
+    public BGMSoundInfos GetInfo(BGMSoundType soundType)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].soundType == soundType)
+            {
+                return sounds[i];
+            }
+        }
+
+        return null;
+    }
+
+    public BGMSoundInfos[] sounds;
 }
