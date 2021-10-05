471f69a8eaa8acc2396c788564a9bdcd6bb60df6
unknown <41745651+dladudqls132@users.noreply.github.com>
2021-10-05 12:35:01 +0900

    로딩씬 추가

--- Assets/Script/LoadingSceneController.cs
+++ Assets/Script/LoadingSceneController.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingSceneController : MonoBehaviour
 {
+    [SerializeField] private Image progressbar;
     private static string nextScene;
- 
+    [SerializeField] private Text text_background;
+    [SerializeField] private Text text;
+    [SerializeField] private GameObject rascal;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,17 +42,53 @@ public class LoadingSceneController : MonoBehaviour
         {
             yield return null;
 
-            if(op.progress>=0.9f)
-            {
-                timer += Time.unscaledDeltaTime;
+            //if (op.progress < 0.9f)
+            //{
+            //    progressbar.fillAmount = op.progress;
+            //}
+            //else
+            //{
+            //    timer += Time.unscaledDeltaTime;
+            //    progressbar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
 
-                if (timer >= 2.0f)
+            //    if (timer >= 2.0f)
+            //    {
+            //        op.allowSceneActivation = true;
+            //        yield break;
+            //    }
+            //}
+
+            if(progressbar.fillAmount < 0.9f)
+            {
+                progressbar.fillAmount = Mathf.Lerp(progressbar.fillAmount, 1.0f, Time.unscaledDeltaTime / 8);
+
+                text_background.text = (progressbar.fillAmount * 100).ToString("N1") + " %";
+                text.text = (progressbar.fillAmount * 100).ToString("N1") + " %";
+
+                rascal.transform.localPosition = new Vector3(-912.0f + (1824 * (progressbar.fillAmount)), -418.7f, 0f);
+                rascal.GetComponent<Animator>().SetFloat("speed", (0.9f - progressbar.fillAmount) * 1.2f);
+            }
+            else
+            {
+                progressbar.fillAmount = Mathf.MoveTowards(progressbar.fillAmount, 1.0f, Time.unscaledDeltaTime / 5);
+
+                text_background.text = (progressbar.fillAmount * 100).ToString("N1") + " %";
+                text.text = (progressbar.fillAmount * 100).ToString("N1") + " %";
+
+                rascal.transform.localPosition = new Vector3(-912.0f + (1824 * (progressbar.fillAmount)), -418.7f, 0f);
+                rascal.GetComponent<Animator>().SetFloat("speed", 1.0f);
+
+                if (progressbar.fillAmount >= 0.99f)
                 {
-                    op.allowSceneActivation = true;
-                    yield break;
+                    timer += Time.unscaledDeltaTime;
+
+                    if(timer >= 1.0f)
+                    {
+                        op.allowSceneActivation = true;
+                        yield break;
+                    }
                 }
             }
-
         }
     }
 }
