742f4fd7a39444bdbd03d13da9b32fdf3d743787
unknown <41745651+dladudqls132@users.noreply.github.com>
2021-10-17 10:37:34 +0900

    크레딧 수정중

---
+++ Assets/UI_CreditController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_CreditController : MonoBehaviour
+{
+    private Animator anim;
+    private float originAnimSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        originAnimSpeed = anim.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.KeypadEnter))
+        {
+            anim.speed = originAnimSpeed * 4;
+        }
+        else
+        {
+            anim.speed = originAnimSpeed;
+        }
+    }
+}
