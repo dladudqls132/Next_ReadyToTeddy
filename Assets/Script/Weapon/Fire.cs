cf248147fecbda936b1db5b01eb52269d0c251c0
DESKTOP-TH8OV4B\임영빈 <dladudqls132@naver.com>
2021-12-01 22:26:00 +0900

    Revert "aa"
    This reverts commit cb2f1abb02fe53fa5f928b2d8099186dc1fb19dc.

--- Assets/Script/Weapon/Fire.cs
+++ Assets/Script/Weapon/Fire.cs
@@ -14,8 +14,9 @@ public class Fire : MonoBehaviour
         if (LayerMask.LayerToName(other.gameObject.layer).Equals("Enemy"))
         {
             Transform other_root = other.GetComponent<Enemy_Bone>().root;
-
             tempNum++;
+
+
 
             if (tempNum % 30 == 0)
             {
@@ -28,23 +29,6 @@ public class Fire : MonoBehaviour
             }
             else
                 other_root.GetComponent<Enemy>().DecreaseHp(damage, other.GetComponent<Collider>().bounds.center, other.transform, this.transform.forward, EffectType.None);
-        }
-        else if(LayerMask.LayerToName(other.gameObject.layer).Equals("InteractiveObject"))
-        {
-            tempNum++;
-
-            if (tempNum % 5 == 0)
-            {
-                GameManager.Instance.GetSoundManager().AudioPlayOneShot(SoundType.Hit);
-                other.GetComponent<InteractiveObject>().DecreaseHp(damage, other.GetComponent<Collider>().bounds.center, other.transform, this.transform.forward, EffectType.Damaged_normal);
-
-                if (!other.GetComponent<InteractiveObject>().GetIsDestroy())
-                    GameManager.Instance.GetCrosshairController().SetAttack_Normal(true);
-                else
-                    GameManager.Instance.GetCrosshairController().SetAttack_Kill(true);
-            }
-            else
-                other.GetComponent<InteractiveObject>().DecreaseHp(damage, other.GetComponent<Collider>().bounds.center, other.transform, this.transform.forward, EffectType.None);
         }
     }
 }
